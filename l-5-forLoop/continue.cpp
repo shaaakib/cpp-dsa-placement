a6bd698b456b4f9f5c7fe67bbabe70dfac7b898d
Muhammad Shakib Hossain <>
2023-08-02 15:10:43 +0600

    Binary & Decimal Number System

---
+++ l-5-forLoop/continue.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0; i<5; i++){
+        cout<<" HI " << endl;
+        cout<<" HEY" << endl;
+        continue;
+        cout<<"Reply toh kor" << endl;
+    }
+}
